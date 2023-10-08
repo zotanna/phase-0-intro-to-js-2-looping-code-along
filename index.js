910f21833e09917f878b84b2099c0631645d564b
denis <denniiskim@gmail.com>
2023-10-08 09:13:45 +0300

    Completed assignmrnt

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-function countDown(startingNumber) {
-  while (startingNumber >= 0) {
+function writeCards(namesArray, eventName) {
+  let messagesArray = [];
+  for (let i = 0; i < namesArray.length; i++) {
+    messagesArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return messagesArray;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+function countdown(message, startingNumber = 10) {
+  while (startingNumber > 0) {
     console.log(startingNumber);
-    startingNumber--;
+    startingNumber = startingNumber - 1;
   }
 }
 
-countDown(10);
+countdown("Launch!");
